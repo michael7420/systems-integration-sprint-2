ad65074608f1a9206eea1b06ab363cf95fb11d16
Wner Mikey <wner_mikey@DESKTOP-LK900SS.localdomain>
2025-12-02 02:15:12 -0600

    Set DEBUG=False and update ALLOWED_HOSTS for EC2

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*km-9%)_80*ib#yx!b5k!d8$b4btjnhstx#ft#vml=m@n554g+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.234.210.30']
 
 
 # Application definition
